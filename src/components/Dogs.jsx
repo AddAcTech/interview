57724f4ffd836daed7bd84841a9298c3b7c092e2
AddAcTech <aguilar.chavez.alexis.daniel@gmail.com>
2023-10-25 16:14:52 -0600

    Dog cards

--- src/components/Dogs.jsx
+++ src/components/Dogs.jsx
@@ -1,14 +1,16 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 function Dogs() {
-  const [dogs, setDogs] = useState(null);
-  const [dogsId, setDogsId] = useState(null);
+  const [dogs, setDogs] = useState("");
+  const [dogArray, setDogArray] = useState("");
+  // const [dogsId, setDogsId] = useState(null);
+
+  //First dog fetch WORKING
   useEffect(() => {
     try {
       const fetchData = async () => {
         const response = await fetch(
           "https://frontend-take-home-service.fetch.com/dogs/search",
           {
-            method: "GET",
             headers: {
               "Content-Type": "application/json",
             },
@@ -17,8 +19,9 @@ function Dogs() {
         );
         const newData = await response.json();
         setDogs(newData);
-        setDogsId(newData.resultIds);
+        //setDogsId(newData.resultIds);
         console.log("Fetched object");
+        console.log(dogs ? dogs.resultIds : "error with dogs array");
       };
       fetchData();
     } catch (error) {
@@ -26,29 +29,40 @@ function Dogs() {
     }
   }, []);
 
-  const getDog = async () => {
-    const responseDog = await fetch(
-      "https://frontend-take-home-service.fetch.com/dogs",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-        body: JSON.stringify(dogs.resultIds),
-      }
-    );
-  };
-  getDog();
-  /* if (dogs) {
-    return (
-      <div>
-        {<p>{dogs.resultIds}</p>}
-      </div>
-    );
-  } else {
-    return null;
-  } */
+  useEffect(() => {
+    //Funcition for object response
+    const getDog = async () => {
+      const responseDog = await fetch(
+        "https://frontend-take-home-service.fetch.com/dogs",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+          body: JSON.stringify(dogs.resultIds),
+        }
+      );
+      const dogObject = await responseDog.json();
+      setDogArray(dogObject);
+      console.log(dogObject);
+    };
+    getDog();
+  }, [dogs]);
+
+  return (
+    <div>
+      {dogArray.map((singleDog) => (
+        <div key={singleDog.id}>
+          <img src={singleDog.img} alt="" />
+          <p>Name: {singleDog.name}</p>
+          <p>Age: {singleDog.age}</p>
+          <p>Breed: {singleDog.breed}</p>
+          <p>Zip code: {singleDog.zip_code}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Dogs;
